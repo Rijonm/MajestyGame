b8bb2cad3022882ae757b6d8222b40dc1b42dc84
Rijon Mushkolaj <rijon.mushkolaj@gmail.com>
2018-09-28 13:32:28 +0200

    changed Testing class

--- MajestyGame/src/Control/Testing.java
+++ MajestyGame/src/Control/Testing.java
@@ -3,7 +3,8 @@ package Control;
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // Coment needed
+		System.out.println("Hi 2");
 
 	}
 
