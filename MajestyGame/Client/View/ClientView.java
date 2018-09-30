db39506d6a416422c2afe31142809e79dd317bde
Rijon Mushkolaj <rijon.mushkolaj@gmail.com>
2018-09-30 17:10:25 +0200

    Buttons added

--- MajestyGame/Client/View/ClientView.java
+++ MajestyGame/Client/View/ClientView.java
@@ -1,6 +1,10 @@
 package View;
 
 import Model.ClientModel;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ClientView{
@@ -11,6 +15,18 @@ public class ClientView{
 	public ClientView(Stage primaryStage, ClientModel model) {
 		this.primaryStage = primaryStage;
 		this.model = model;
+		
+		BorderPane loginPage = new BorderPane();
+		Button spielStartenButton = new Button("Spiel starten");
+		Button loginButton = new Button ("Login");
+		VBox startOptionBox = new VBox();
+		
+		startOptionBox.getChildren().addAll(spielStartenButton, loginButton);
+		loginPage.setCenter(startOptionBox);
+		
+		Scene scene = new Scene(loginPage);
+		
+		primaryStage.setScene(scene);
 		
 		
 	}
