7c9d548ca2bdc2d46033eddfcebd16c49e8d8d30
ozanf <ozanf@10.174.31.163>
2018-10-01 18:49:34 +0200

    Loeschen

--- MajestyGame/Client/View/ClientView.java
+++ MajestyGame/Client/View/ClientView.java
@@ -19,7 +19,7 @@ public class ClientView{
 		BorderPane loginPage = new BorderPane();
 		Button spielStartB = new Button("Spiel starten");
 		Button loginB = new Button ("Login");
-		Button accountOptionsB = new Button("Registrieren/Löschen");
+		Button accountOptionsB = new Button("Registrieren/Loeschen");
 		Button einstellungenB = new Button("Einstellungen");
 		Button beendenB = new Button("Beenden");
 		VBox startOptionBox = new VBox();
