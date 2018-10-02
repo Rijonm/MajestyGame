f20a4e6a56f42c4eeafd0b614337ea3484d37d2f
Rijon Mushkolaj <rijon.mushkolaj@gmail.com>
2018-10-02 23:03:29 +0200

    Button handling

--- MajestyGame/Client/Control/ClientController.java
+++ MajestyGame/Client/Control/ClientController.java
@@ -2,12 +2,71 @@ package Control;
 
 import Model.ClientModel;
 import View.ClientView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 
 public class ClientController {
+	
+	public ClientModel model;
+	public ClientView view;
 
 	public ClientController(ClientModel model, ClientView view) {
+		this.model = model;
+		this.view = view;
+		
+		// ---------------------LOGIN-BUTTON
+		view.loginB.setOnAction(new EventHandler<ActionEvent> (){
+			
+			public void handle(ActionEvent event) {
+				
+				view.primaryStage.setScene(view.setSecondScene());
+				view.start();
+				
+				
+			}
+		});
+		// ---------------------LOGIN-BUTTON
+		
+		// --------------------- REGISTRIEREN-BUTTON
+		view.registrierenB.setOnAction(new EventHandler<ActionEvent> (){
+			
+			public void handle(ActionEvent event) {
+				
+				
+			}
+		});
+		
+		// --------------------- REGISTRIEREN-BUTTON
+		
+		// ---------------------START-BUTTON
+		view.startGame.setOnAction(new EventHandler<ActionEvent> (){
+					
+			public void handle(ActionEvent event) {
+						
+				view.primaryStage.setScene(view.setThirdScene());
+				view.start();
+						
+						
+			}
+		});
+		// ---------------------START-BUTTON
+		
+		
+		
+		
+//		view.loginB.setOnAction(new EventHandler<ActionEvent> (){
+//			
+//			public void handle(ActionEvent event) {
+		
+//				// ------WRITE HERE WHAT TO HANDLE -------
+//			}
+//		});
+		
 		
 		
 	}
+	
+	
 
 }
