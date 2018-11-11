f370b37b707930fdc3c624c7aa91c312c010fcb1
memek55 <mert.emek@students.fhnw.ch>
2018-11-11 19:29:06 +0100

    Card Class, Soldier, Defense, Farmer, Brewer, Queen

---
+++ MajestyGame/Client/View/CardBrewer.java
@@ -0,0 +1,24 @@
+package View;
+
+public class CardBrewer extends Cards{
+	
+	//code by Mert Emek
+
+	public CardBrewer(String CardName) {
+		super(CardName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void defend() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ MajestyGame/Client/View/CardSoldier.java
@@ -0,0 +1,33 @@
+package View;
+
+public class CardSoldier extends Cards{
+	
+	//code by Mert Emek
+
+	public CardSoldier(String CardName) {
+		super(CardName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void defend() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//code by Mert Emek
+	
+	
+	
+	
+	
+	
+	
+
+}
